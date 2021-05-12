1cac2983bb39e08007c0ecbee86698e858fcabb1
Mbielovas <mbielovas@gmail.com>
2021-05-12 12:41:38 -0500

    finished js file

---
+++ main.js
@@ -0,0 +1,17 @@
+
+//colors array
+let colors = ['blue', 'yellow', 'black', 'red', 'brown', 'orange'];
+
+//get button
+let button = document.getElementById('button');
+
+//even listener
+button.addEventListener('click', function(){
+    //randomizer
+    var randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+    //get container
+    let container = document.getElementById('container');
+
+    container.style.background = randomColor;
+})
